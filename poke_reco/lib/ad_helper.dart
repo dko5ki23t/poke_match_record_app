0449815d35d94e9b95c638e0ccd721f3d4a5fde4
dko5ki23t <dko5.ki23t@gmail.com>
2024-05-15 21:12:40 +0900

    広告用コード元に戻した

--- poke_reco/lib/ad_helper.dart
+++ poke_reco/lib/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-5298352571661081/6928639305';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
