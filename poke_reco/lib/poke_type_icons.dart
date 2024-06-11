243731d593b95516086c39b3454807f71a084af8
dko5ki23t <dko5.ki23t@gmail.com>
2024-06-12 00:21:18 +0900

    タイプアイコンのサイズ（余白）をGoogleIconsに合わせて調整

--- poke_reco/lib/poke_type_icons.dart
+++ poke_reco/lib/poke_type_icons.dart
@@ -21,20 +21,20 @@ class PokeTypeIcons {
   static const _kFontFam = 'PokeType';
   static const String? _kFontPkg = null;
 
-  static const IconData ice =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData psychic =
-      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData evil =
-      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData dragon =
-      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData fairy =
-      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData fly =
-      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData ghost =
-      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ice =
+      IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData terastal =
-      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData psychic =
+      IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData evil =
+      IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dragon =
+      IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData fly =
+      IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData fairy =
+      IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
