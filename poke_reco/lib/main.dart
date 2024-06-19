af4910b2b66a0bce458a7952143826bd636d82f1
dko5ki23t <dko5.ki23t@gmail.com>
2024-06-19 17:01:23 +0900

    v2.1.2(対戦記録表示の修正)

--- poke_reco/lib/main.dart
+++ poke_reco/lib/main.dart
@@ -27,7 +27,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 
-const String pokeRecoVersion = '2.1.1';
+const String pokeRecoVersion = '2.1.2';
 const int pokeRecoInternalVersion = 4; // SQLのテーブルバージョンに使用
 
 enum TabItem {
